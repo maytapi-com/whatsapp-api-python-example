87361b6e7e01cd65ea29a174618f30a37521392d
Maytapi <maytapi.com@gmail.com>
2024-07-16 13:51:05 +0300

    python example added

---
+++ app.py
@@ -0,0 +1,182 @@
+from pickle import TRUE
+from urllib import response
+from flask import Flask, request, jsonify
+from pyngrok import ngrok
+import requests
+import sys
+import base64
+import json
+
+app = Flask(__name__)
+
+# Replace the values here.
+INSTANCE_URL = "https://api.maytapi.com/api"
+PRODUCT_ID = ""
+PHONE_ID = ""
+API_TOKEN = ""
+
+
+@app.route("/")
+def hello():
+    return app.send_static_file("index.html")
+
+
+def getCatalog():
+    url = INSTANCE_URL + "/" + PRODUCT_ID + "/" + PHONE_ID + "/catalog"
+    payload = {}
+    headers = {
+        "Content-Type": "application/json",
+        "x-maytapi-key": API_TOKEN,
+    }
+    r = requests.request('GET', url, headers=headers, data=payload)
+    tjson = r.json()
+    pData = tjson["data"]
+    pSuccess = tjson["success"]
+    if pSuccess == True and len(pData) > 0:
+        pId = tjson["data"][0]["productId"]
+        return pId
+    else:
+        return 0
+
+
+def send_response(body):
+    print("Request Body", body, file=sys.stdout, flush=True)
+    url = INSTANCE_URL + "/" + PRODUCT_ID + "/" + PHONE_ID + "/sendMessage"
+    headers = {
+        "Content-Type": "application/json",
+        "x-maytapi-key": API_TOKEN,
+    }
+    response = requests.post(url, json=body, headers=headers)
+    print("Response", response.json(), file=sys.stdout, flush=True)
+    return
+
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    json_data = request.get_json()
+
+    wttype = json_data["type"]
+    if wttype == "message":
+        message = json_data["message"]
+        conversation = json_data["conversation"]
+        _type = message["type"]
+        if message["fromMe"]:
+            return
+        if _type == "text":
+            # Handle Messages
+            text = message["text"]
+            text = text.lower()
+            if text == "media":
+                body = {
+                    "type": "media",
+                    "text": "Image Response",
+                    "message": "https://via.placeholder.com/140x100",
+                }
+            elif text == "media64":
+                with open("maytapi.jpg", "rb") as img_file:
+                    b64_string = base64.b64encode(img_file.read())
+                firstStr = "data:image/jpeg;base64,"
+                formatImg = b64_string.decode('utf-8')
+                fullDataImg = firstStr+formatImg
+                body = {
+                    "type": "image",
+                    "text": "image Response",
+                    "message": fullDataImg
+                }
+            elif text == "location":
+                body = {
+                    "type": "location",
+                    "text": "Location Response",
+                    "latitude": "41.093292",
+                    "longitude": "29.061737",
+                }
+            elif text == "link":
+                body = {
+                    "type": "link",
+                    "message": "https://maytapi.com/",
+                }
+            elif text == "contact":
+                body = {
+                    "type": "contact",
+                    "message": "905301234567@c.us",
+                }
+            elif text == "vcard":
+                body = {
+                    "type": "vcard",
+                    "message": {
+                        "displayName": "John Doe",
+                        "vcard": "BEGIN:VCARD\nVERSION:3.0\nFN;CHARSET=UTF-8:John Doe\nN;CHARSET=UTF-8:;John;Doe;;\nTEL;TYPE=CELL:+9051234567\nREV:2020-01-23T11:09:14.782Z\nEND:VCARD",
+                    },
+                }
+            elif text == "filedoc":
+                body = {
+                    "type": "media",
+                    "message": "https://file-examples-com.github.io/uploads/2017/02/file-sample_100kB.doc",
+                }
+            elif text == "filepdf":
+                body = {
+                    "type": "media",
+                    "message": "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+                }
+            elif text == "reply":
+                rpyto = json_data["message"]["_serialized"]
+                body = {
+                    "type": "text",
+                    "message": "This is reply text",
+                    "reply_to": rpyto
+                }
+            elif text == "product":
+                productIdTake = getCatalog()
+                if productIdTake == 0:
+                    body = {
+                        "type": "text",
+                        "message": "You don't have a any product"
+                    }
+                else:
+                    body = {
+                        "type": "product",
+                        "productId": productIdTake
+                    }
+            else:
+                body = {"type": "text", "message": "Echo - " + text}
+            body.update({"to_number": conversation})
+            send_response(body)
+    elif wttype == "status":
+        stype =  json_data["type"]
+        spid =  json_data["pid"]
+        sphoneid =  json_data["phone_id"]
+        sStatus =  json_data["status"]
+        print("Status of instance ", sphoneid, " changed to ", sStatus)
+    elif wttype == "ack":
+        ackType =  json_data["type"]
+        ackProductId =  json_data["product_id"]
+        ackData =  json_data["data"]
+        acty = ackData[0]
+        acmsgId = ackData[0]
+        print("your message with id ",
+              acmsgId["msgId"], " was ", acty["ackType"])
+    else:
+        print("Unknow Type:", wttype,  file=sys.stdout, flush=True)
+    return jsonify({"success": True}), 200
+
+
+def setup_webhook():
+    if PRODUCT_ID == "" or PHONE_ID == "" or API_TOKEN == "":
+        print(
+            "You need to change PRODUCT_ID, PHONE_ID and API_TOKEN values in app.py file.", file=sys.stdout, flush=True
+        )
+        return
+    public_url = ngrok.connect(9000)
+    url = INSTANCE_URL + "/" + PRODUCT_ID + "/setWebhook"
+    print("url", url, file=sys.stdout, flush=True)
+    headers = {
+        "Content-Type": "application/json",
+        "x-maytapi-key": API_TOKEN,
+    }
+    body = {"webhook": public_url.public_url + "/webhook"}
+    response = requests.post(url, json=body, headers=headers)
+    print("webhook ", response.json())
+
+
+# Do not use this method in your production environment
+setup_webhook()
---
+++ readme.md
@@ -0,0 +1,58 @@
+# Maytapi - WhatsApp Api Bot Example
+
+> A simple python bot for Maytapi WhatsApp Api
+
+[Our Website](https://maytapi.com/) • [WhatsApp Api Documentations](https://maytapi.com/whatsapp-api-documentation) • [WhatsApp Api Postman Collection](https://maytapi.com/whatsapp-postman-documentation)
+
+- In this example we echo the text messages back to user.
+- We use ngrok to create temporary https reverse proxy so whatsapp can reach our demo api.
+- Because ngrok public url changes everytime we also change webhook settings in our instance at boot. This should not be used like this in production environments.
+- NOTE: Before testing the demo you need to create your phone instance and connect an active WhatsApp account to instance in [Phones Page](https://console.maytapi.com/).
+
+# Installation
+
+### Installing python libraries
+
+`pip install -r requirements.txt`
+
+### Configure Tokens
+
+You need to change PRODUCT_ID, PHONE_ID and API_TOKEN values in app.py file. You can find your Product ID and Token in [Settings Token Page](https://console.maytapi.com/settings/token). Phone Id can be found in [Phones Page](https://console.maytapi.com/) or with `/listPhones` endpoint.
+
+# Start The Api
+
+Tested with python v3.8.1
+
+## Mac/Linux
+
+```
+export FLASK_APP=app.py
+flask run --host 0.0.0.0 --port 9000 --no-debugger --no-reload
+```
+
+## Windows Cmd
+
+```
+set FLASK_APP=app.py
+flask run --host 0.0.0.0 --port 9000 --no-debugger --no-reload
+```
+
+## Windows PowerShell
+
+```
+$env:FLASK_APP = "app.py"
+flask run --host 0.0.0.0 --port 9000 --no-debugger --no-reload
+```
+
+You can test this bot with these preconfigured messages:
+
+- media
+- media64
+- location
+- reply
+- link
+- filepdf
+- filedoc
+- vcard
+- contact
+- product
